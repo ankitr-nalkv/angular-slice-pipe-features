bbc91c83322d6c0da65a9f12605b182eb8979232
ankitr-nalkv <ankitr.nalkv@gmail.com>
2020-10-11 12:00:29 +0530

    Creating initial scenarios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,31 @@
-import { Component, VERSION } from '@angular/core';
-import { of } from 'rxjs';
+import { Component, OnDestroy, OnInit, VERSION } from '@angular/core';
+import { of, Subscription } from 'rxjs';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
+export class AppComponent implements OnInit, OnDestroy {
   name = 'Angular ' + VERSION.major;
-  data = of([1,2,3,4,5]);
-  controlData = of ([
+  data = of(['A', 'B', 'C', 'D', 'E']);
+  firstEle: string;
+  private subscription = new Subscription();
+
+  ngOnInit(): void {
+    this.subscription.add(this.data.subscribe(ele => {
+      this.firstEle = ele[0];
+    }));
+  };
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+
+
+
+  controlData = of([
     {
       label: 'sliderprogress',
       value: 50
@@ -19,7 +35,7 @@ export class AppComponent  {
       value: 40
     }
   ])
-  chartData = of ([
+  chartData = of([
     {
       name: 'clothes',
       value: 100
